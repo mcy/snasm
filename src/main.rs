72bfdc5767c87f0b23569c3958753ee9ffa4fabf
Miguel Young de la Sota <mcyoung@mit.edu>
2020-05-19 18:09:57 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//! SNASM, a SNES assembler/disassembler and patch generation tool.
+
+#![deny(missing_docs)]
+#![deny(unused)]
+#![deny(warnings)]
+#![deny(unsafe_code)]
+
+fn main() {
+    println!("Hello, world!");
+}
